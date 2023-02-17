c88a12ca086ee83e6c4b792da941d91df03dcfff
Orionspeed <harborlarinsteven@gmail.com>
2023-02-17 10:35:44 -0800

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * print all letters except q and e
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar (alp);
+
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar (alp);
+
+	putchar('\n');
+	return (0);
+}
