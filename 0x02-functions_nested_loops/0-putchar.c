57fc644239aa26532905d1231ec7654d75201c5a
Orionspeed <harborlarinsteven@gmail.com>
2023-02-21 02:20:29 -0800

    alx_low_level_programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0(Succes)
+ */
+
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; x[i]; i++)
+		_putchar(x[i]);
+
+	_putchar('\n');
+	return (0);
+}
