5c630e1989bc8738e3a4a7cbcf2c9d1a02d6a4ba
Orionspeed <harborlarinsteven@gmail.com>
2023-02-27 04:10:49 -0800

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,10 +3,9 @@
 /**
  * print_array - a function that prints n elements of an array
  * @a: array name
- * @n: is the number of elements of the array to be printed
+ * @n: is the number of elements OF the array to be printed
  * Return: a and n inputs
  */
-
 void print_array(int *a, int n)
 {
 	int i;
@@ -15,9 +14,9 @@ void print_array(int *a, int n)
 	{
 		printf("%d, ", a[i]);
 	}
-	if (i == (n - 1))
-	{
-		printf("&d", a[n - 1]);
-	}
-	printf("\n");
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
